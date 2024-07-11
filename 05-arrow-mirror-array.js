6d3b5ae67678c8955d7b2d0fd221fafa2364d19c
ViridianPlexus <viridian@Viridian>
2024-07-11 21:58:09 +0200

    changes

--- 05-arrow-mirror-array.js
+++ 05-arrow-mirror-array.js
@@ -10,7 +10,20 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here 
+arrowMirrorArray=(arr)=>{
+
+  newArr = [];
+  intArr = arr.toReversed()
+
+  newArr = arr.concat(intArr);
+
+  return newArr
+
+
+}
+
+console.log(arrowMirrorArray([1,2,3]));
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
